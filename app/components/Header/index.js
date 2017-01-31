f1d9d8e6f5e26d3a39b4324d1e321807d9bbea1d
Tim Myers <timmyers09@gmail.com>
2017-01-30 18:14:45 -0700

    CORRECTLY remove the about link

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -11,6 +11,10 @@ const Header = () => (
 		<HorizontalFlex width="20%">
 		</HorizontalFlex>
 
+		<HorizontalFlex width="60%">
+			<HeaderLink to="/">Home</HeaderLink>
+		</HorizontalFlex>
+
 		<HorizontalFlex width="20%">
 			<HeaderLink to="/login">Log In</HeaderLink>
 		</HorizontalFlex>
