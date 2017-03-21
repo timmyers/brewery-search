ab8579efc3b07ecc36eb22c56052bec401bef33a
Tim Myers <timmyers09@gmail.com>
2017-03-20 22:02:52 -0600

    More test for HeaderLink

--- app/components/Header/components/tests/HeaderLink.test.js
+++ app/components/Header/components/tests/HeaderLink.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react';
 import chai from 'chai';
 import { shallow } from 'enzyme';
@@ -10,12 +11,27 @@ import HeaderLink from '../HeaderLink';
 chai.should();
 
 describe('<HeaderLink />', () => {
-  it('should render a flat button inside a link', () => {
+  it('should render a FlatButton inside a Link', () => {
     const wrapper = shallow(<HeaderLink />);
 
     const linkWrapper = wrapper.find(Link);
     linkWrapper.should.have.length(1);
 
     linkWrapper.children().find(FlatButton).should.have.length(1);
+  });
+
+  it('should pass the \'to\' prop to the Link component', () => {
+    const to = '/home';
+    const wrapper = shallow(<HeaderLink to={to} />);
+    wrapper.props().to.should.equal(to);
+  });
+
+  it('should render the children', () => {
+    const wrapper = shallow(
+      <HeaderLink>
+        <span className="unique" />
+      </HeaderLink>
+    );
+    wrapper.contains(<span className="unique" />).should.be.true;
   });
 });
