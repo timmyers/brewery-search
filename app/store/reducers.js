9d906fe86e2a07d5776df2d90de933e18eab2492
Tim Myers <timmyers09@gmail.com>
2017-03-21 20:53:15 -0600

    delete login reducer reference

--- app/store/reducers.js
+++ app/store/reducers.js
@@ -2,14 +2,12 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
 import { reducer as apiReducer } from 'api';
-import { reducer as loginReducer } from 'routes/Login';
 import { reducer as mapReducer } from 'routes/Home';
 
 const makeRootReducer = asyncReducers => (
   combineReducers({
     form: formReducer,
     api: apiReducer,
-    login: loginReducer,
     map: mapReducer,
     ...asyncReducers,
   })
